d6fc59d05e83afaf81d98ff852ffc7f0542f09b1
Unam <97320894+manukao@users.noreply.github.com>
2023-02-28 11:47:53 +0100

    add parentheses

--- components/Pentagon/Pentagon.js
+++ components/Pentagon/Pentagon.js
@@ -3,7 +3,7 @@ import getRandomColor from "../../utils/randomColor.js";
 export default function Pentagon(color) {
   const pentagon = document.createElement("div");
   pentagon.classList.add("pentagon");
-  pentagon.style.backgroundColor = getRandomColor;
+  pentagon.style.backgroundColor = getRandomColor();
   pentagon.addEventListener("click", () => {
     pentagon.style.backgroundColor = "#ccc";
   });
