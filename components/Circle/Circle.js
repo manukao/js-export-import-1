da7b1c91a51d1d7e029c94cfae4cc23e3c57ff1a
Unam <97320894+manukao@users.noreply.github.com>
2023-02-28 11:47:31 +0100

    add parentheses

--- components/Circle/Circle.js
+++ components/Circle/Circle.js
@@ -3,7 +3,7 @@ import getRandomColor from "../../utils/randomColor.js";
 export default function Circle() {
   const circle = document.createElement("div");
   circle.classList.add("circle");
-  circle.style.backgroundColor = getRandomColor;
+  circle.style.backgroundColor = getRandomColor();
   circle.addEventListener("click", () => {
     circle.style.backgroundColor = "#ccc";
   });
