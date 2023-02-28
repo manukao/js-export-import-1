5fc4ad35e091b8452b5c9079af374d0ca6160f1a
Unam <97320894+manukao@users.noreply.github.com>
2023-02-28 11:48:08 +0100

    add parentheses

--- components/Square/Square.js
+++ components/Square/Square.js
@@ -3,7 +3,7 @@ import getRandomColor from "../../utils/randomColor.js";
 export default function Square(color) {
   const square = document.createElement("div");
   square.classList.add("square");
-  square.style.backgroundColor = getRandomColor;
+  square.style.backgroundColor = getRandomColor();
   square.addEventListener("click", () => {
     square.style.backgroundColor = "#ccc";
   });
